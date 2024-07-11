0bf5b95537b5f20de2165b3623c1e8813bb004dc
Rob Scherer <robscherer6@gmail.com>
2024-07-11 16:51:09 -0400

    added pseudo backend api for jobs + spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
-import jobs from '../jobs.json'
+import Spinner from './Spinner'
 
 const JobListings = ({ isHome = false }) => {
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/jobs');
+        const data = await res.json();
+        setJobs(data); 
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -10,11 +27,14 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          { jobListings.map((job) => (
+          { loading ? (<Spinner loading={loading}/> 
+          ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            { jobs.map((job) => (
             <JobListing key={job.id} job={ job }/>
           ))}
-        </div>
+          </div>
+          )}
       </div>
     </section>
   )
